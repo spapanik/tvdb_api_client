828589c2c2e74e2b21738f68ebe5180203e9e01e
steph papanik <spapanik21@gmail.com>
2016-09-27 15:11:59 +0100

    initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This project is a python client for the TVDB API.
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+PKG_NAME = 'tvdb_api_client'
+__version__ = '0.0.0a0'
+
+
+def listify(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+setup(
+    name=PKG_NAME,
+    packages=[PKG_NAME],
+    version=__version__,
+    description='A python client for TVDB rest API',
+    license='MIT',
+    long_description=contents('README.txt'),
+    author='Stephanos Papanikolopoulos',
+    author_email='spapanik21@gmail.com',
+    url='https://github.com/spapanik/{pkg_name}'.format(pkg_name=PKG_NAME),
+    download_url='https://github.com/spapanik/{pkg_name}/tarball/{ver}'.format(
+        pkg_name=PKG_NAME,
+        ver=__version__,
+    ),
+    keywords=listify('KEYWORDS.txt'),
+    classifiers=listify('CLASSIFIERS.txt'),
+)
