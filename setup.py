1850a8bc1410a06ce61cc6a793aa073fe3c3e4a8
Steph Papanik <spapanik21@gmail.com>
2018-10-06 14:29:05 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 __author__ = "spapanik"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __license__ = "license"
 PKG_NAME = "tvdb_api_client"
 
@@ -13,15 +13,14 @@ def contents(filename):
 
 setup(
     name=PKG_NAME,
-    packages=[PKG_NAME],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     version=__version__,
     description="A python client for TVDB rest API",
     license=__license__,
     long_description=contents("readme.md"),
     author="Stephanos Papanikolopoulos",
     author_email="spapanik21@gmail.com",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
     url=f"https://github.com/{__author__}/{PKG_NAME}",
     download_url="https://github.com/{__author__}/{PKG_NAME}/tarball/{__version__}",
     python_requires=">=3.6",
