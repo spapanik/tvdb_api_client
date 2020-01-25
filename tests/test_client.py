a305ffe2432f422e9a77dae13f54052c48248878
Stephanos Kuma <spapanik21@gmail.com>
2020-01-25 18:04:50 +0000

    Add test for _Cache

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,15 @@
 from tvdb_api_client import client
 
 
+class TestCache:
+    @staticmethod
+    def test_set():
+        cache = client._Cache()
+        cache.set("attribute", "value")
+        assert cache.get("attribute") == "value"
+
+
 class TestTVDBClient:
     @staticmethod
-    def test_init():
-        client.TVDBClient("username", "key", "api_key", {})
+    def test_init_without_cache():
+        client.TVDBClient("username", "key", "api_key")
