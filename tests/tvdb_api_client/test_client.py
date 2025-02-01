56b25976373a476c7914f17c88e4e6bdbe1db818
Stephanos Kuma <stephanos@kuma.ai>
2025-02-01 08:45:05 +0000

    🔬 Test the different init options

--- tests/tvdb_api_client/test_client.py
+++ tests/tvdb_api_client/test_client.py
@@ -1,3 +1,8 @@
+import os
+from unittest import mock
+
+import pytest
+
 from tvdb_api_client import client
 
 
@@ -11,5 +16,20 @@ class TestCache:
 
 class TestTVDBClient:
     @staticmethod
-    def test_init_without_cache() -> None:
+    @mock.patch(
+        "tvdb_api_client.client.get_setting",
+        new=mock.MagicMock(return_value=None),
+    )
+    def test_init() -> None:
+        with pytest.raises(ValueError, match="API Key is required."):
+            client.TheTVDBClient()
+        with pytest.raises(ValueError, match="API Key is required."):
+            client.TheTVDBClient(pin="1234")
+        client.TheTVDBClient(api_key="1234")
+        client.TheTVDBClient(api_key="1234", pin="1234")
+
+    @staticmethod
+    def test_init_from_env_vars() -> None:
+        os.environ["TVDB_API_KEY_V4"] = "1234"
+        os.environ["TVDB_PIN_V4"] = "1234"
         client.TheTVDBClient()
